9ff948622c909d936a96444f610b3ef17b5490fd
Anthony <a898954139@gmail.com>
2023-06-23 16:56:17 +0800

    ADD Register View

--- Scheduling/Controllers/AccountController.cs
+++ Scheduling/Controllers/AccountController.cs
@@ -18,5 +18,9 @@ namespace Scheduling.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
--- Scheduling/Utility/Helper.cs
+++ Scheduling/Utility/Helper.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Scheduling.Helper
+namespace Scheduling.Utility
 {
     public static class Helper
     {
